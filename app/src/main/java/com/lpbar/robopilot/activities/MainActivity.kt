0e213eed7b0cbf3cb7aeff66311e2d9a209bef08
Leszek Barszcz <leszek.barszcz@accenture.com>
2019-03-25 20:21:21 +0100

    Steering pad moved to independent fragment

--- app/src/main/java/com/lpbar/robopilot/activities/MainActivity.kt
+++ app/src/main/java/com/lpbar/robopilot/activities/MainActivity.kt
@@ -1,34 +1,45 @@
 package com.lpbar.robopilot.activities
 
+import android.content.Context
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.AttributeSet
+import android.view.View
+import android.widget.TextView
 import com.lpbar.robopilot.R
+import com.lpbar.robopilot.fragments.SteeringFragment
 
 import com.lpbar.robopilot.services.*
 
 import kotlinx.android.synthetic.main.activity_main.*
 import okhttp3.Response
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SteeringFragment.OnFragmentInteractionListener {
+    companion object {
+        private var serverAddress: String = "192.168.0.213"
+            set(value) { networkService.address = value }
 
-    private var serverAddress: String = "192.168.0.213"
-        set(value) { networkService.address = value }
-
-    private var serverPort: String = "9090"
-        set(value) { networkService.port = value }
-
-    private var networkService: NetworkServiceInterface = NetworkService(serverAddress, serverPort)
+        private var serverPort: String = "9090"
+            set(value) { networkService.port = value }
+        var networkService: NetworkServiceInterface = NetworkService(serverAddress, serverPort)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        openJoyPad.setOnClickListener { onOpenJoypad() }
         stopMotorsButton.setOnClickListener { onStopMotors() }
         stopExplorationButton.setOnClickListener { onStopExploration() }
         startExplorationButton.setOnClickListener { onStartExploration() }
         sendManualPoseButton.setOnClickListener { onManualPose() }
+
+        ipAddressEditText.setText(serverAddress, TextView.BufferType.EDITABLE)
+        portEditText.setText(serverPort, TextView.BufferType.EDITABLE)
+
         ipAddressEditText.addTextChangedListener( object: TextWatcher {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
 
@@ -51,23 +62,6 @@ class MainActivity : AppCompatActivity() {
                 serverPort = p0.toString()
             }
         })
-        joystick.setOnMoveListener({ angle, strength ->
-            if (strength == 0) {
-                networkService.sendManualMotorAction(0.0, 0.0)
-                return@setOnMoveListener
-            }
-
-            var angleNorm = 0.0
-            var strengthNorm: Double = strength / 100.0
-            if (angle < 180) {
-                angleNorm = angle - 90.0
-            } else {
-                angleNorm = 270.0 - angle
-                strengthNorm *= -1
-            }
-
-            networkService.sendManualMotorAction(angleNorm / 90.0, strengthNorm)
-        }, 100)
     }
 
     private fun onStopMotors() {
@@ -89,12 +83,21 @@ class MainActivity : AppCompatActivity() {
                 callback = {printResponse(it)}, error = {printMessage(it)})
     }
 
+    private fun onOpenJoypad() {
+        val joypadFragment = SteeringFragment.newInstance()
+        joypadFragment.show(supportFragmentManager, "joypadFgm")
+    }
+
     private fun printResponse(response: Response) {
         runOnUiThread {
             val consoleText = consoleOutputTextView.text
             val responseString = response.toString()
             consoleOutputTextView.text = "$responseString\n$consoleText"
         }
+    }
+
+    override fun onFragmentInteraction(uri: Uri) {
+
     }
 
     private fun printMessage(message: String) {
--- app/src/main/java/com/lpbar/robopilot/fragments/SteeringFragment.kt
+++ app/src/main/java/com/lpbar/robopilot/fragments/SteeringFragment.kt
@@ -3,15 +3,17 @@ package com.lpbar.robopilot.fragments
 import android.content.Context
 import android.net.Uri
 import android.os.Bundle
-import android.support.v4.app.Fragment
+import android.support.v4.app.DialogFragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
 import com.lpbar.robopilot.R
+import com.lpbar.robopilot.activities.MainActivity
 import com.lpbar.robopilot.services.NetworkServiceInterface
 import io.github.controlwear.virtual.joystick.android.JoystickView
 import kotlinx.android.synthetic.main.fragment_steering.*
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -27,13 +29,12 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  *
  */
-class SteeringFragment : Fragment() {
-    private var networkService: NetworkServiceInterface? = null
+class SteeringFragment : DialogFragment() {
+    private var networkService: NetworkServiceInterface = MainActivity.networkService
+    var listener: OnFragmentInteractionListener? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -42,5 +43,72 @@ class SteeringFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_steering, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        joystick.setOnMoveListener({ angle, strength ->
+            if (strength == 0) {
+                networkService.sendManualMotorAction(0.0, 0.0)
+                return@setOnMoveListener
+            }
+
+            var angleNorm = 0.0
+            var strengthNorm: Double = strength / 100.0
+            if (angle < 180) {
+                angleNorm = angle - 90.0
+            } else {
+                angleNorm = 270.0 - angle
+                strengthNorm *= -1
+            }
+
+            networkService.sendManualMotorAction(angleNorm / 90.0, strengthNorm)
+        }, 100)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is OnFragmentInteractionListener) {
+            listener = context
+        } else {
+            throw RuntimeException(context.toString() + " must implement OnFragmentInteractionListener")
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     *
+     *
+     * See the Android Training lesson [Communicating with Other Fragments]
+     * (http://developer.android.com/training/basics/fragments/communicating.html)
+     * for more information.
+     */
+    interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        fun onFragmentInteraction(uri: Uri)
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment BlankFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance() =
+                SteeringFragment().apply {
+                    arguments = Bundle().apply { }
+                }
+    }
 }
